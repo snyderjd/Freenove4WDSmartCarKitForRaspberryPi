36835a3fd355d4a14dd733e63066b366d2c012b4
Joe Snyder <snyderjosephd@yahoo.com>
2025-01-07 21:29:36 -0600

    Add bluetooth server code to remote_control

--- Code/Server/remote_control.py
+++ Code/Server/remote_control.py
@@ -17,7 +17,35 @@ class RemoteControl:
         GPIO.setup(self.echo_pin, GPIO.IN)
 
     def open_socket_and_control_car():
-        print("opening socket so client can control car...")
+        # bt_server
+        # The address of Raspberry Pi bluetooth adapter on the server. The server might have multiple bluetooth adapters
+        hostMACAddress = "D8:3A:DD:70:92:5D"
+        port = 1 # Port must be set explicitly (typically 1 for Bluetooth RFCOMM)
+        backlog = 1
+        size = 1024
+
+        # Create a bluetooth socket
+        server_sock = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
+        server_sock.bind((hostMACAddress, port))
+        server_sock.listen(backlog)
+
+        print(f"Listening on {hostMACAddress}, port {port}")
+
+        try:
+            client_sock, client_info = server_sock.accept()
+            print(f"Accepted connection from {client_info}")
+
+            while True:
+                data = client_sock.recv(size)
+                if data:
+                    print("Received: ", data.decode("utf-8"))
+                    client_sock.send(data) # Echo back the received data
+        except Exception as e:
+            print(f"Error: {e}")
+        finally:
+            print("Closing sockets")
+            client_sock.close()
+            server_sock.close()
 
 remote_control = RemoteControl()
 
